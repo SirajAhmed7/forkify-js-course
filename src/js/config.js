2cd9e18fab7114eb74f6e7188426af1b4a74c156
SirajAhmed7 <sirajahmedkhatri32@gmail.com>
2022-09-16 12:44:58 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// File for constants and reusable variables
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+// export const API_KEY = 'b1de7f90-8512-41f1-9528-894105880a15';
+export const KEY = '8efbcd2a-616c-4ded-861b-b93ee8daa429';
+export const MODAL_CLOSE_SEC = 2.5;
